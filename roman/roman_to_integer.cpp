c7d9d7731dd6729065474429be23d14e600dd39f
fredrikWHaug <fredrikwhaug@gmail.com>
2023-06-18 06:38:52 -0400

    added hash map for key value relationship between numerals and integers

--- roman/roman_to_integer.cpp
+++ roman/roman_to_integer.cpp
@@ -1,1 +1,19 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <unordered_map>
 
+class Solution {
+public:
+    std::unordered_map<std::string, int> numerals;
+
+    int romanToInt(string s) {
+        numerals.insert({"I", 1});
+        numerals.insert({"V", 5});
+        numerals.insert({"X", 10});
+        numerals.insert({"L", 50});
+        numerals.insert({"C", 100});
+        numerals.insert({"D", 500});
+        numerals.insert({"M", 1000});
+    }
+};
