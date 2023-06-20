12790252b33c9de5581274df555f4643ebcb9a95
fredrikWHaug <fredrikwhaug@gmail.com>
2023-06-20 08:16:18 -0400

    roman_to_integer.cpp

--- roman/roman_to_integer.cpp
+++ roman/roman_to_integer.cpp
@@ -1,11 +1,55 @@
 #include <string>
+#include <unordered_map>
 
 class Solution {
 public:
     int romanToInt(string s) {
-        char string_split[s.size()]; 
-    }
-};
+
+        int result = 0;
+
+        // exceptions
+        std::string four = "IV";
+        std::string nine = "IX";
+        std::string forty = "XL";
+        std::string ninety = "XC";
+        std::string four_h = "CD";
+        std::string nine_h = "CM";
+
+        if (s == four) {
+            return 4;
+        } else if (s == nine) {
+            return 9;
+        } else if (s == forty) {
+            return 40;
+        } else if (s == ninety) {
+            return 90;
+        } else if (s == four_h) {
+            return 400;
+        } else if (s == nine_h) {
+            return 900;
+        }
+
+        for(int i = 0; i < s.size(); i ++) {
+            if (s[i] == 'I') {
+                result ++;
+            } else if (s[i] == 'V') {
+                result += 5;
+            } else if (s[i] == 'X') {
+                result += 10;
+            } else if (s[i] == 'L') {
+                result += 50;
+            } else if (s[i] == 'C') {
+                result += 100;
+            } else if (s[i] == 'D') {
+                result += 500;
+            } else {
+                result += 1000;
+            }
+            }
+            return result;
+        }
+
+    };
 
 /*
 Pseudo:
