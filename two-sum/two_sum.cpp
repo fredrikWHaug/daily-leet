e2d29ccbae07b4a64bb372d4891a2b0643e5a371
fredrikWHaug <fredrikwhaug@gmail.com>
2023-06-28 06:01:55 -0400

    trying to store the ith element and check it with all elements in the list

--- two-sum/two_sum.cpp
+++ two-sum/two_sum.cpp
@@ -1,1 +1,20 @@
 
+#include <vector>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int result = 0;
+        for (int i = 0; i < nums.size(); i ++) {
+            int test = nums[i];
+            for (int j = 0; j < nums.size(); j ++) {
+                result = test + nums[j];
+                if (result == target) {
+                    return result;
+                }
+            }
+        }
+
+
+    }
+};
