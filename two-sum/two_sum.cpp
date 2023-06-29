68a885389bdd2e9ba416d7dddaec6c65d8de1094
fredrikWHaug <fredrikwhaug@gmail.com>
2023-06-29 06:50:41 -0400

    trying to store the ith element in the vector, then adding the proceeding jth alement to see if the sum mathces the target. I seem to have some vector technical problems right now, but the logic honestly makes sense to me. However, it might very well be wrong.

--- two-sum/two_sum.cpp
+++ two-sum/two_sum.cpp
@@ -1,20 +1,21 @@
-
+#include <iostream>
 #include <vector>
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        int result = 0;
+        std::vector result;
         for (int i = 0; i < nums.size(); i ++) {
             int test = nums[i];
-            for (int j = 0; j < nums.size(); j ++) {
-                result = test + nums[j];
-                if (result == target) {
-                    return result;
+            for (int j = i+1; j < nums.size(); j ++) {
+                int check = test + nums[j];
+                if (check == target) {
+                    result.push_back(nums[i]);
+                    result.push_back(nums[j]);
                 }
             }
         }
-
-
+        return result;
     }
 };
