be741bc92c6a6e0e1b00f84b53c40633c9246e2a
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-01 08:10:05 -0400

    trying to reverse the list, then check if the reversed list has the same content and order as the initial argument that is passed in

--- easy/palindrome/palindrome.cpp
+++ easy/palindrome/palindrome.cpp
@@ -1,1 +1,26 @@
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        std:vector<int> numbers;
+        for (int i = 0; i < x; i ++) {
+            numbers.push_back(i);
+        };
+
+        vector<int> reverse_numbers = reverse(numbers);
+        for (const auto& number : reverse_numbers) {
+            std::cout << number << " ";
+        }
+        std::cout << std::endl;
+        return 0;
+    }
+
+    static vector<int> reverse(std::vector<int> list) {
+        return std::reverse(list.begin(), list);
+    }
+};
