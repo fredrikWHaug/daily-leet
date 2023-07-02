3eb2cb0c140a46fef11e72e8d78beeacfa310dec
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-02 09:18:59 -0400

    trying to reverse the argument but realized it is an int and can't iterate over it, so need more thinking.

--- easy/palindrome/palindrome.cpp
+++ easy/palindrome/palindrome.cpp
@@ -1,26 +1,45 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
+// this works
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
-        std:vector<int> numbers;
-        for (int i = 0; i < x; i ++) {
-            numbers.push_back(i);
-        };
 
-        vector<int> reverse_numbers = reverse(numbers);
-        for (const auto& number : reverse_numbers) {
-            std::cout << number << " ";
+        std::vector<int> my_list = {1, 2, 3};
+
+        std::reverse(my_list.begin(), my_list.end());
+
+        for (const auto& element : my_list) {
+            std::cout << element << " ";
         }
         std::cout << std::endl;
-        return 0;
-    }
-
-    static vector<int> reverse(std::vector<int> list) {
-        return std::reverse(list.begin(), list);
+        return true;
     }
 };
+
+// here is a way to split an integer into a list of individual characters
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    int number = 12345;
+    std::string numberString = std::to_string(number);
+    std::vector<char> digits;
+
+    for (char c : numberString) {
+        digits.push_back(c);
+    }
+
+    // Print the split digits
+    for (char digit : digits) {
+        std::cout << digit << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
