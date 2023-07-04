4d9a42298df42418f2f9689815651c850cbc509a
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-04 08:31:41 -0400

    keepping track of whether ith letter is the same after defining ith letter with a boolean. Should work I think.

--- easy/longest-common-prefix/longest-common-prefix.cpp
+++ easy/longest-common-prefix/longest-common-prefix.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        bool same_letter_check = true;
+        for (int i = 0; i < strs.size(); i ++) {
+            std::string prefix = "";
+            for (int j = 0; j < strs.size(); j ++) {
+                if 
+            }
+        }
+    }
+};
