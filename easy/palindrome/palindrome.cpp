5f482af77047e7c5b8c36e7ce22ff476629538c0
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-04 08:18:36 -0400

    3 problems down! Just copied a version of the list I made of the strings I made and compared the reversed copy to the non-reversed list

--- easy/palindrome/palindrome.cpp
+++ easy/palindrome/palindrome.cpp
@@ -12,11 +12,11 @@ public:
         for (char c : string_version) {
             string_list.push_back(std::string(1, c));
         }
+        vector<std::string> copy = string_list;
+        std::reverse(copy.begin(), copy.end());
 
-        vector<std::string> reversed_list = std::reverse(string_list.begin(), string_list.end());
-
-        for (int i = 0; i < reversed_list.size(); i ++) {
-            if (reversed_list[i] != string_list[i]) {
+        for (int i = 0; i < copy.size(); i ++) {
+            if (copy[i] != string_list[i]) {
                 result = false;
             }
         }
