36d2a9502f6af6c6a20c2d2a6aef2ed5e5597f2f
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-05 09:59:36 -0400

    trying to store the ith letter of the first word, and see if the ith letter of every other word matches. Might be onto something with this nested loop. However, important that I index [0][i] which was today's change.

--- easy/longest-common-prefix/longest-common-prefix.cpp
+++ easy/longest-common-prefix/longest-common-prefix.cpp
@@ -1,12 +1,17 @@
+#include <string>
+#include <iostream>
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        bool same_letter_check = true;
+        bool empty = false;
+        std::string longest_prefix = "";
         for (int i = 0; i < strs.size(); i ++) {
-            std::string prefix = "";
-            for (int j = 0; j < strs.size(); j ++) {
-                if 
+            std::string prefix = strs[0][i];
+            for (int j = 0; j < strs.size(); i ++) {
+                // see if the first letter of the rest of the words match?
             }
         }
+
+
     }
 };
