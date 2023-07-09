b21416ccb2696702c4a1e026dd25413e50627bc2
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-09 05:37:38 -0400

    Opening session for first intermediate problem, integer to roman numeral.

--- medium/integer_to_roman/integer_to_roman.cpp
+++ medium/integer_to_roman/integer_to_roman.cpp
@@ -1,1 +1,24 @@
+#import <iostream>
+#import <map>
 
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        std::vector<std::string> number_string;
+
+        for (int i = 0; num[i] != NULL)
+
+        std::map<int, std::string> int_numeral_map;
+
+        int_numeral_map[1] = "I";
+        int_numeral_map[5] = "V";
+        int_numeral_map[10] = "X";
+        int_numeral_map[50] = "L";
+        int_numeral_map[100] = "C";
+        int_numeral_map[500] = "D";
+        int_numeral_map[1000] = "M";
+
+        for (int i = 0; i < )
+    }
+};
