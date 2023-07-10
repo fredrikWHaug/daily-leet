4b623b145d4c60c070f4c1ada0a04b4b1f7dc476
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-10 08:24:33 -0400

    I think it is a mistake to make a list of strings, because I think having access to the digit value is useful when determining what numeral to use.

--- medium/integer_to_roman/integer_to_roman.cpp
+++ medium/integer_to_roman/integer_to_roman.cpp
@@ -5,10 +5,14 @@ class Solution {
 public:
     string intToRoman(int num) {
 
-        std::vector<std::string> number_string;
+        std::string number_string = std::to_string(num);
 
-        for (int i = 0; num[i] != NULL)
+        std::vector<std::string> digits;
 
+        for (char digit : number_string) {
+            digits.push_back(std::string(1, digit));
+        }
+        
         std::map<int, std::string> int_numeral_map;
 
         int_numeral_map[1] = "I";
@@ -19,6 +23,14 @@ public:
         int_numeral_map[500] = "D";
         int_numeral_map[1000] = "M";
 
-        for (int i = 0; i < )
+        std::vector<std::string> roman_numerals;
+        for (int i = 0; i < digits.size(); i ++) {
+            if (digits[i] <= 5) {
+                roman_numerals.push_back(digits[i]*int_numeral_app[1]);
+            }
+        }
+
+
+        return "1";
     }
 };
