922b85cf5a3d971797db2aebd0ecbc93cf50b104
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-11 07:23:22 -0400

    Currently working on converting initial int argument into a list of characters and then converting the characters back to integers but remain in a vector so I can iterate over the numbers and compare them with the map structure for the numerals and make a numeral out of them. I think that is certainly doable but I am not yet sure how to handle the special cases of subtraction, but I am sure I can take inspiration from my previous solution to the numeral to integer problem.

--- medium/integer_to_roman/integer_to_roman.cpp
+++ medium/integer_to_roman/integer_to_roman.cpp
@@ -7,30 +7,33 @@ public:
 
         std::string number_string = std::to_string(num);
 
-        std::vector<std::string> digits;
+        std::vector<char> digit_chars;
 
         for (char digit : number_string) {
-            digits.push_back(std::string(1, digit));
-        }
-        
-        std::map<int, std::string> int_numeral_map;
-
-        int_numeral_map[1] = "I";
-        int_numeral_map[5] = "V";
-        int_numeral_map[10] = "X";
-        int_numeral_map[50] = "L";
-        int_numeral_map[100] = "C";
-        int_numeral_map[500] = "D";
-        int_numeral_map[1000] = "M";
-
-        std::vector<std::string> roman_numerals;
-        for (int i = 0; i < digits.size(); i ++) {
-            if (digits[i] <= 5) {
-                roman_numerals.push_back(digits[i]*int_numeral_app[1]);
-            }
+            digit_chars.push_back(digit);
         }
 
+        // for (std::string digit : digit_chars) {
+        //     int digit = digit -'0';
+        // }
 
-        return "1";
+        // std::map<int, std::string> int_numeral_map;
+
+        // int_numeral_map[1] = "I";
+        // int_numeral_map[5] = "V";
+        // int_numeral_map[10] = "X";
+        // int_numeral_map[50] = "L";
+        // int_numeral_map[100] = "C";
+        // int_numeral_map[500] = "D";
+        // int_numeral_map[1000] = "M";
+
+        // std::vector<std::string> roman_numerals;
+        // for (int i = 0; i < digits.size(); i ++) {
+        //     if (digits[i] <= 5) {
+        //         roman_numerals.push_back(digits[i]*int_numeral_app[1]);
+        //     }
+        // }
+
+        return "69";
     }
 };
