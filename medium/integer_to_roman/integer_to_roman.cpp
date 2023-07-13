fd5c1d88b94164e7c9075ff958333b3877a7b3a3
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-12 23:44:26 -0400

    Some pretty substantial improvements on the solution right there. Just time, intuition and fluency to be honest. I am not thinking thorugh the steps I am taking and trying to actively learn or memorize anything. I am just trying out things that I think would make sense to do, but I think I should probably try to incorporate a more structured approach to these probems. I did manage to pass test 1 however, so all that remains now is to finish the conditionals in the outer loop where I push roman numerals to the vector I created and make sure I pass the edge and special cases. I estimate 60 more minutes on this problem to be honest. So I think the only way to drastically improve the efficiency and time spent on problems is if I have done a similar problem before and can recognize what to do specifically. It will take time to get there, but hopefully, the time it takes to solve a problem will decrease over time.

--- medium/integer_to_roman/integer_to_roman.cpp
+++ medium/integer_to_roman/integer_to_roman.cpp
@@ -1,54 +1,44 @@
-#import <iostream>
-#import <map>
+#include <iostream>
+#include <map>
 
 class Solution {
 public:
     string intToRoman(int num) {
         
         std::string number_string = std::to_string(num);
+
         int number_length = number_string.length();
         int number_array[number_length];
 
         for (int i = 0; i < number_length; i ++) {
             char number_char = number_string[i];
-            int number = static_cast<int>(number_char);
+            int number = number_char - '0';
             number_array[i] = number;
         }
 
+        std::map<int, char> int_numeral_map;
+
+        int_numeral_map[1] = 'I';
+        int_numeral_map[5] = 'V';
+        int_numeral_map[10] = 'X';
+        int_numeral_map[50] = 'L';
+        int_numeral_map[100] = 'C';
+        int_numeral_map[500] = 'D';
+        int_numeral_map[1000] = 'M';
+
+        std::vector<char> roman_numerals;
         for (int i = 0; i < number_length; i ++) {
-            std::cout << number_array[i] << std:: endl;
+            if (number_array[i] <= 5) {
+                for (int j = 0; j < number_array[i]; j ++) {
+                    roman_numerals.push_back(int_numeral_map[1]);
+                }
+            }
         }
+        std::string result;
 
-        // for (char digit : number_string) {
-        //     digit_chars.push_back(digit);
-        //     std::cout << digit << std::endl;
-        // }
-        // for (char digit : digit_chars) {
-        //     digit = static_cast<int>(digit);
-        //     digit = digit - '0';
-        // }
-
-        // for (int digit : digit_chars) {
-        //     std::cout << digit << std::endl;
-        // }
-
-        // std::map<int, std::string> int_numeral_map;
-
-        // int_numeral_map[1] = "I";
-        // int_numeral_map[5] = "V";
-        // int_numeral_map[10] = "X";
-        // int_numeral_map[50] = "L";
-        // int_numeral_map[100] = "C";
-        // int_numeral_map[500] = "D";
-        // int_numeral_map[1000] = "M";
-
-        // std::vector<std::string> roman_numerals;
-        // for (int i = 0; i < digits.size(); i ++) {
-        //     if (digits[i] <= 5) {
-        //         roman_numerals.push_back(digits[i]*int_numeral_app[1]);
-        //     }
-        // }
-
-        return "69";
+        for (int i = 0; i < roman_numerals.size(); i ++) {
+            result += roman_numerals[i];
+        }
+        return result;
     }
 };
