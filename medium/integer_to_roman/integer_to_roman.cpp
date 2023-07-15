12ca7e9562e803d010496ec1b1461a9a1a9875a2
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-15 06:33:33 -0400

    Today's Contribution.

--- medium/integer_to_roman/integer_to_roman.cpp
+++ medium/integer_to_roman/integer_to_roman.cpp
@@ -27,30 +27,42 @@ public:
         int_numeral_map[500] = 'D';
         int_numeral_map[1000] = 'M';
 
-        [2, 4, 5, 9]
-
         int exponentiation = number_length - 1;
         std::vector<char> roman_numerals;
         for (int i = 0; i < number_length; i ++) {
-            number_array[i] = number_array[i]*pow(10, exponentiation); 
-            if (number_array[i] >= 1 && number_array[i] < 5) {
-                
-            } else if (number_array[i] >= 5 && number_array[i] < 10) {
-
-            } else if (number_array[i] >= 10 && number_array[i] < 50) {
-
-            } else if (number_array[i] >= 50 && number_array[i] < 100) {
-
-            } else if (number_array[i] >= 100 && number_array[i] < 500) {
-                
-            } else if (number_array[i] >= 500 && number_array[i] < 1000) {
-
+            int power = number_array[i]*pow(10, exponentiation); 
+            if (power >= 1 && power < 5) {
+                for (int j = 0; j < number_array[i]; j ++){
+                    roman_numerals.push_back(int_numeral_map[1]);
+                }
+            } else if (power >= 5 && power < 10) {
+                for (int j = 0; j < number_array[i]; j ++) {
+                    roman_numerals.push_back(int_numeral_map[5]);
+                }
+            } else if (power >= 10 && power < 50) {
+                for (int j = 0; j < number_array[i]; j ++) {
+                    roman_numerals.push_back(int_numeral_map[10]);
+                }
+            } else if (power >= 50 && power < 100) {
+                for (int j = 0; j < number_array[i]; j++) {
+                    roman_numerals.push_back(int_numeral_map[50]);
+                }
+            } else if (power >= 100 && power < 500) {
+                for (int j = 0; j < number_array[i]; j ++) {
+                    roman_numerals.push_back(int_numeral_map[100]);
+                }
+            } else if (power >= 500 && power < 1000) {
+                for (int j = 0; j < number_array[i]; j ++) {
+                    roman_numerals.push_back(int_numeral_map[500]);
+                }
             } else {
-                
+                for (int j = 0; j < number_array[i]; j ++) {
+                    roman_numerals.push_back(int_numeral_map[1000]);
+                }
             }
         }
+        
         std::string result;
-
         for (int i = 0; i < roman_numerals.size(); i ++) {
             result += roman_numerals[i];
         }
