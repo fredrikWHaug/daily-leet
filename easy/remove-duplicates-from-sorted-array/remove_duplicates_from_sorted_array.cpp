5cfe54a0f038bc8716872cdc457a08b78aa8c652
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-25 04:03:00 -0400

    removing duplicates

---
+++ easy/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        // remove duplicates in nums and keep track of
+        // the number of unique elements
+
+        
+
+    }
+};
