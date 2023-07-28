a5d5e0a335e3bd3cb3ff0862895f1afadddf8070
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-28 06:21:23 -0400

    3rd attempt. Problem 7 solved.

--- easy/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.cpp
+++ easy/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        // start at the second index
-        // check if the index is equal to the previous index
-        // if it is not, we have a unique digit so ... hmm
+        // please look into the last index duplication problem
 
-        for (int i = 0; i < nums.size(); i ++) {
-            
+        int j = 1;
+        for (int i = 1; i < nums.size(); i ++) {
+            if (nums[i] != nums[i-1]) {
+                nums[j] = nums[i];
+                j ++;
+            }
         }
+        return j;
     }
 };
