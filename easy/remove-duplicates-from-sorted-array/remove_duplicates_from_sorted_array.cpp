612200a7d4dd0a1f6258334613621ede0320d9d4
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-07-28 06:23:14 -0400

    Update remove_duplicates_from_sorted_array.cpp

--- easy/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.cpp
+++ easy/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-
         // please look into the last index duplication problem
-
         int j = 1;
         for (int i = 1; i < nums.size(); i ++) {
             if (nums[i] != nums[i-1]) {
