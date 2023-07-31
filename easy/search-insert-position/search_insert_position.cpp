1e7619df4aad9c65e265009324175d0657d7dcf6
fredrikWHaug <fredrikwhaug@gmail.com>
2023-07-31 07:13:21 -0400

    attempt 1

--- easy/search-insert-position/search_insert_position.cpp
+++ easy/search-insert-position/search_insert_position.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int return_value;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (nums[i] == target) {
+                return_value = i;
+            }
+        }
+        return return_value;
+    }
+};
