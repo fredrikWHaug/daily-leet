f11242581f2da0fb93f81d9b749c8e9bc30e4b24
fredrikWHaug <fredrikwhaug@gmail.com>
2023-08-02 07:18:35 -0400

    3rd attempt (completely by myself)

--- easy/search-insert-position/search_insert_position.cpp
+++ easy/search-insert-position/search_insert_position.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int return_value;
         for (int i = 0; i < nums.size(); i ++) {
             if (nums[i] == target) {
                 return i;
-            } else if (target < nums[i]) {
-                return i;
-            } else {
-                return nums.size();
             }
         }
-        return 0;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (target < nums[i]) {
+                return i;
+            }
+        }
+        return nums.size();
     }
 };
