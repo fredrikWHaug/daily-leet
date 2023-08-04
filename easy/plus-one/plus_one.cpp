6dce698b57533969ef857abf6bb75419e8b00904
fredrikWHaug <fredrikwhaug@gmail.com>
2023-08-04 09:37:17 -0400

    attempt 2

--- easy/plus-one/plus_one.cpp
+++ easy/plus-one/plus_one.cpp
@@ -1,30 +1,19 @@
 #include <cmath>
-
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        // I say safest way is to record this
-        // as an actual integer, add one, and
-        // populate a new vector
-
-        int digit = 0;
+        // make exponent for tenths place
         int exponent = digits.size() - 1;
+
+        int total_value = 0;
         for (int i = 0; i < digits.size(); i ++) {
-            digit += digits[i] * pow(10, exponent);
+            digit += digits[i]*pow(10, exponent);
             exponent --;
         }
-        digit = digit + 1;
+        int new_total_value = digit + 1;
+        // invariant: new_total_value is the total value we want
 
-        int new_length = digits.size();
-        if (digits.back() == 9) {
-            new_length += 1;
-        }
-
-        vector<int> new_digit;
-        for (int i = 0; i < new_length; i ++) {
-            new_digit.push_back(digit[i]);
-        }
-
-        return new_digit;
+        std::cout << new_digit << std::endl;
+        return digits;
     }
 };
