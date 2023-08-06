7967082cf10660db87b3c1462fc2639f2b1997bb
fredrikWHaug <fredrikwhaug@gmail.com>
2023-08-06 18:58:37 -0400

    attempt 4 :)

--- easy/plus-one/plus_one.cpp
+++ easy/plus-one/plus_one.cpp
@@ -1,18 +1,19 @@
-#include <cmath>
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        for (int i = n-1; i >= 0; i --) {
-            if (i == n-1) {
+        for (int i = n - 1; i >= 0; i --) {
+            if (i == n - 1) {
                 digits[i] ++;
-            } else if (digits[i] == 10) {
+            }
+            if (digits[i] == 10) {
                 digits[i] = 0;
-            } else if (i!= 0) {
-                digits[i-1] ++;
-            } else {
-                digits.push_back(0);
-                digits[i] = 1;
+                if (i != 0) {
+                    digits[i - 1] ++;
+                } else {
+                    digits.push_back(0);
+                    digits[i] = 1;
+                }
             }
         }
         return digits;
