00870251fb9d4ee915acf5dc35efcbda64335b5f
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-08-11 09:08:57 -0400

    Create integer_to_roman.cpp

---
+++ medium/integer-to-roman/integer_to_roman.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+#include <string>
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string thousands[] = {"", "M", "MM", "MMM"};
+
+        return thousands[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+    }
+};
