81f226e42e774adb53adacb721b14c8d52d746b0
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-08-20 13:00:22 +0200

    Update pascals_triangle.cpp

--- easy/pascals-triangle/pascals_triangle.cpp
+++ easy/pascals-triangle/pascals_triangle.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
+
+    vector<int> generateRow(int row) {
+        long long ans = 1;
+        vector<int> rowResult;
+        rowResult.push_back(1);
+        for (int i = 1; i < row; i ++) {
+            ans = ans * (row - i);
+            ans = ans / (i);
+            rowResult.push_back(ans);
+        }
+        return rowResult;
+    }
+
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> triangle;
-        vector<int> row1 = {1, 1};
-        triangle.push_back(row1);
-        std::cout << row1 << std::endl;
+        for (int i = 1; i <= numRows; i ++) {
+            triangle.push_back(generateRow(i));
+        }
         return triangle;
     }
 };
