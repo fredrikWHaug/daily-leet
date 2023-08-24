559468f7ffd693fe654b0f63d5c74c6fe7fb7138
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-08-24 13:50:19 +0200

    Update max_stock_profit.cpp

--- easy/max-stock-profit/max_stock_profit.cpp
+++ easy/max-stock-profit/max_stock_profit.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int lowest_price = prices[0];
-        int highest_price = prices[0];
-        for (int i = 1; i < prices.size(); ++i) {
-            if (prices[i] < lowest_price) {
-                lowest_price = prices[i];
-            }
-        }
-        // invariant: lowest_price containts the lowest price in the list
+        int max_profit = 0;
+        int min_price = prices[0];
 
-        for (int i = 1; i < prices.size(); ++i) {
-            if (prices[i] > highest_price) {
-                highest_price = prices[i];
-            }
+        for (int i = 0; i < prices.size(); ++i) {
+            max_prof = max(max_prof, prices[i] - min_price);
+            min_price = min(prices[i], min_price);
         }
-        // invariant: highest price contains the highest price in the list
-    return highest_price - lowest_price;
+
+        return max_prof;
     }
 };
