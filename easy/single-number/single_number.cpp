5abbfbf034d696bc523d531fc2986970c5531a0f
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-08-25 13:59:45 +0200

    Create single_number.cpp

---
+++ easy/single-number/single_number.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int candidate = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            if 
+        }
+    }
+};
