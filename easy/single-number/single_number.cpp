df553a91fe799e3b381db9b116b9b80f46343fbc
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-08-26 06:02:05 +0200

    Update single_number.cpp

--- easy/single-number/single_number.cpp
+++ easy/single-number/single_number.cpp
@@ -2,9 +2,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int candidate = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            if 
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i){
+            mp[nums[i]] ++;
         }
+        for (auto m : mp) {
+            if (m.second == 1) {
+                return m.first;
+            }
+        }
+        return -1;
     }
 };
