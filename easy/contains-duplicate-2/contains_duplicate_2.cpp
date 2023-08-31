5a25749f8ae1b87c1bf5f697ffdceb666e3d784c
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-08-31 10:18:12 +0200

    Update contains_duplicate_2.cpp

--- easy/contains-duplicate-2/contains_duplicate_2.cpp
+++ easy/contains-duplicate-2/contains_duplicate_2.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
+        unordered_map<int, int> = mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (count) {
+                if (abs(mp[nums[i]] <= k)) {
+                    mp[nums[i]] = i;
+                }
+            }
+            return false;
+        }
     }
 };
