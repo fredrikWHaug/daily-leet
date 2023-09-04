2820053cde259c8f606c0a709bdcf4a5cb763726
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-04 11:40:09 -0400

    Create summary_ranges.cpp

---
+++ easy/summary-ranges/summary_ranges.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> output_vector;
+
+        for (int i = 0; i : nums.size() ++i) {
+             
+         } 
+    }
+};
