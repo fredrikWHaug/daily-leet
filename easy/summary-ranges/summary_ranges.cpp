8bc2d2c63fa50f0c67ac3399caafe273b70cbfdd
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-08 07:34:01 -0400

    Update summary_ranges.cpp

--- easy/summary-ranges/summary_ranges.cpp
+++ easy/summary-ranges/summary_ranges.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> result;
-        if (nums.size() == 0) return result;
-        int a = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (a == nums.size() - 1 || a != nums[i] + 1) {
-                
+        int start_range = nums[0];
+        int n = nums.size();
+        if (n == 0) return result;
+        for (int i; i < nums.size(); ++i) {
+            if (i == nums.size() || nums[i] != nums[i + 1]) {
+                if (nums[i] != start_range) {
+                    result.push_back(to_string(start_range) + "->" + to_string(nums[i]));
+                } else {
+                    result.push_back(to_string(start_range));
+                }
+                if (i != n-1) {
+                    start_range = nums[i + 1];
+                }
             }
         }
-        }
+        return result;
     }
 };
