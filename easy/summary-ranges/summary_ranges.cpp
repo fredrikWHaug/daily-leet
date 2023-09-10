7fae4b19ac78feced312aab410625fbb58a128f2
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-10 08:44:05 -0400

    Update summary_ranges.cpp

--- easy/summary-ranges/summary_ranges.cpp
+++ easy/summary-ranges/summary_ranges.cpp
@@ -6,15 +6,7 @@ public:
         int n = nums.size();
         if (n == 0) return result;
         for (int i; i < nums.size(); ++i) {
-            if (i == nums.size() || nums[i] != nums[i + 1]) {
-                if (nums[i] != start_range) {
-                    result.push_back(to_string(start_range) + "->" + to_string(nums[i]));
-                } else {
-                    result.push_back(to_string(start_range));
-                }
-                if (i != n-1) {
-                    start_range = nums[i + 1];
-                }
+
             }
         }
         return result;
