47cc1d6970d2413ecf48b21bf01baae8c8736a21
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2024-02-09 22:47:15 -0500

    Minor fix, trying to figure out how to not predefine position as zero

--- array/ant_boundary.py
+++ array/ant_boundary.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ant_pos = 0
-        boundary_crossings = -1
-        for i in range(nums):
-            if ant_pos == 0:
-                boundary_crossings += 1
-            if nums[i] > 0:
-                ant_pos += 1
-            elif nums[i]< 0:
-                ant_pos -= 1
-        return boundary_crossings
+        pos = 0
+        boundary_hits = 0
+        for i in range(len(nums)):
+            pos += i
+            if pos == 0:
+                boundary_hits += 1
+        return boundary_hits
